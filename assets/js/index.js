4f629977837abd6dc81c04f9a0d01454741d98da
dongmenqin <2518719426@qq.com>
2021-08-22 12:35:54 +0800

    完成个人中心功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,6 @@ $(function () {
             //     Authorization: localStorage.getItem("token") || ""
             // },
             success(res) {
-                console.log(res);
                 if (res.status != 0) {
                     return layui.layer.msg("获取用户信息失败")
                 }
